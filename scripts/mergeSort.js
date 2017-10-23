ff9d8afa86154a63996ecfe88887e4fec1115b39
Julie Dursema <jmdursema@gmail.com>
2017-10-23 13:41:00 -0600

    import to index.js

--- scripts/mergeSort.js
+++ scripts/mergeSort.js
@@ -31,32 +31,3 @@ function merge(a,b){
 
 
 module.exports = mergeSort;
-
-function mergeSort(array){
-	if (array.length <= 1){
-		return array;
-	}
-	const midpoint = Math.floor(array.length/2);
-	const a = array.slice(0, midpoint);
-	const b = array.slice(midpoint, array.length)
-
-	return merge (mergeSort(a), mergeSort(b));
-}
-
-function merge (a, b){
-	while (a.length && b.length)
-		if (a[0]<= b[0]){
-			results.push(a.shift());
-		}else{
-			results.push(b.shift());
-		}
-
-		while(a.length){
-			results.push(a.shift());
-		}
-		while(b.length){
-			results.push(b.shift());
-		}
-
-		return results;
-}
